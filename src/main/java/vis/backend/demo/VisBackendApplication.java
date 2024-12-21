d30373df51403a05e870ed3c7bc84196dbec4102
persi <jina8151010@gmai.com>
2024-12-21 13:22:43 +0900

    chore: 메인 클래스 추가

---
+++ src/main/java/vis/backend/demo/VisBackendApplication.java
@@ -0,0 +1,13 @@
+package vis.backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vis/backend/demo/VisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vis.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
