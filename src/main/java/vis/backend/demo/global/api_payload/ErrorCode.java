03265502dbe4f64bc508c3f93e3a767a326ea046
persi <jina8151010@gmai.com>
2024-12-22 13:16:53 +0900

    chore: error, success code 파일 추가

---
+++ src/main/java/vis/backend/demo/global/api_payload/ErrorCode.java
@@ -0,0 +1,31 @@
+package vis.backend.demo.global.api_payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode implements BaseCode {
+
+    // Common
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 에러, 서버 개발자에게 문의하세요."),
+    INVALID_PARAMETER(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5002", "잘못된 DAY Parameter 가 들어왔습니다. "),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .httpStatus(this.httpStatus)
+                .isSuccess(false)
+                .code(this.code)
+                .message(this.message)
+                .build();
+    }
+}
---
+++ src/main/java/vis/backend/demo/global/api_payload/SuccessCode.java
@@ -0,0 +1,30 @@
+package vis.backend.demo.global.api_payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessCode implements BaseCode {
+
+    // Common
+    OK(HttpStatus.OK, "COMMON_200", "Success"),
+    CREATED(HttpStatus.CREATED, "COMMON_201", "Created"),
+
+   ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .httpStatus(this.httpStatus)
+                .isSuccess(true)
+                .code(this.code)
+                .message(this.message)
+                .build();
+    }
+}
