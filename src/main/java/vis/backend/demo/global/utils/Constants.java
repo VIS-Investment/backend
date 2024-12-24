bb49ad15ad84918c103202f3bf97092cae1c2d9a
persi <jina8151010@gmai.com>
2024-12-24 17:02:36 +0900

    chore: 비밀번호 정규식 추가

--- src/main/java/vis/backend/demo/global/utils/Constants.java
+++ src/main/java/vis/backend/demo/global/utils/Constants.java
@@ -3,5 +3,6 @@ package vis.backend.demo.global.utils;
 public class Constants {
     // 정규식
     public static final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_.=+~-])(?=\\S+$).{8,20}$";
 
 }
