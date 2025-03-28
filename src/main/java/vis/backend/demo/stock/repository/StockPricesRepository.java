e45d668daeb4bc2f08ad7be99c6ee44d7abd5509
persi <jina8151010@gmai.com>
2025-03-28 20:44:36 +0900

    feat: StockPrices 및 StockInfo 엔티티의 Repository 생성

--- src/main/java/vis/backend/demo/stock/repository/StockPricesRepository.java
+++ src/main/java/vis/backend/demo/stock/repository/StockPricesRepository.java
@@ -1,8 +1,10 @@
 package vis.backend.demo.stock.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vis.backend.demo.stock.domain.StockPrices;
 
 public interface StockPricesRepository extends JpaRepository<StockPrices, Long> {
-
+    Optional<StockPrices> findByStockInfo_TickerAndTradeDate(String ticker, LocalDate tradeDate);
 }
