17ff6a23f314ede9d529b3f8de1e39fcf9430c12
persi <jina8151010@gmai.com>
2025-04-10 15:46:23 +0900

    refactor: 데이터 적재용 엔티티 StockPrices로 변경

--- src/main/java/vis/backend/demo/stock/strategy/FetchStrategy.java
+++ src/main/java/vis/backend/demo/stock/strategy/FetchStrategy.java
@@ -2,10 +2,10 @@ package vis.backend.demo.stock.strategy;
 
 import java.util.List;
 import vis.backend.demo.stock.domain.StockInfo;
-import vis.backend.demo.stock.domain.StockPricesCompositeIdx;
+import vis.backend.demo.stock.domain.StockPrices;
 
 public interface FetchStrategy {
-    List<StockPricesCompositeIdx> fetch(List<StockInfo> infos, String range);
+    List<StockPrices> fetch(List<StockInfo> infos, String range);
 
     String getType(); // "virtual" or "webclient"
 }
